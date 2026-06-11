7b82a989c5a04b6cecb0e30cebbe6a3e5b98a5e6
Nha1213 <Vothanarern@gmail.com>
2026-06-11 15:35:10 +0700

    category

--- src/main/java/com/nha/nha_smos/Controller/CategoryController.java
+++ src/main/java/com/nha/nha_smos/Controller/CategoryController.java
@@ -1,25 +1,51 @@
 package com.nha.nha_smos.Controller;
 
 import ch.qos.logback.core.model.Model;
+import com.nha.nha_smos.DTO.CategoryRequest;
+import com.nha.nha_smos.DTO.CategoryResponse;
 import com.nha.nha_smos.Service.CategoryService;
+import jdk.jfr.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 //@RequiredArgsConstructor
 @Component
-@RequestMapping("api/category")
+@RestController
+@RequestMapping("/api/category")
 public class CategoryController {
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
     public CategoryController(CategoryService categoryService){
         this.categoryService = categoryService;
     }
 
+
 //    @GetMapping
-//    public String getCategories(Model model){}
+//    public ResponseEntity<List<CategoryResponse>> findAll(){
+//        return ResponseEntity.ok();
+//    }
+
+//    @PostMapping
+//    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest dto  ){
+//        return ResponseEntity.ok(this.categoryService.save(dto));
+//    }
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody CategoryRequest dto  ){
+        Map<String,Object> res = new HashMap<>();
+        res.put("success", true);
+        res.put("code", HttpStatus.OK.value());
+        res.put("message", "Category created successfully");
+        res.put("errors", null);
+        res.put("data", this.categoryService.save(dto));
+        return ResponseEntity.status(HttpStatus.CREATED.value()).body(res);
+    }
 
 
 }
--- src/main/java/com/nha/nha_smos/Service/CategoryService.java
+++ src/main/java/com/nha/nha_smos/Service/CategoryService.java
@@ -1,23 +1,36 @@
 package com.nha.nha_smos.Service;
 
 import com.nha.nha_smos.DTO.CategoryRequest;
+import com.nha.nha_smos.DTO.CategoryResponse;
+import com.nha.nha_smos.Mapper.CategoryMapper;
 import com.nha.nha_smos.Model.CategoryModel;
 import com.nha.nha_smos.Repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
-    public CategoryModel save(CategoryRequest dto) {
-        CategoryModel  category = new CategoryModel();
-        category.setName(dto.getName());
-        category.setDescription(dto.getDescription());
-        category.setCode(dto.getCode());
-        category.setStatus(dto.getStatus());
-        return this.categoryRepository.save(category);
+    // Display data category
+    public List<CategoryResponse> gitList(){
+
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::toResponse)
+                .toList();
+    }
+
+
+    // Insert into Data Category
+    public CategoryResponse save(CategoryRequest dto) {
+        CategoryModel category = categoryMapper.toEntity(dto);
+        CategoryModel savedCategory = categoryRepository.save(category);
+        return categoryMapper.toResponse(savedCategory);
     }
 }
