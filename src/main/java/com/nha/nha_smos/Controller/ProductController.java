d6719131338f6b0a9fa9c4de81273b8bec02e9a4
Nha1213 <Vothanarern@gmail.com>
2026-07-02 16:37:56 +0700

    Update ProductController 2.1

--- src/main/java/com/nha/nha_smos/Controller/ProductController.java
+++ src/main/java/com/nha/nha_smos/Controller/ProductController.java
@@ -1,17 +1,21 @@
 package com.nha.nha_smos.Controller;
 
+import com.nha.nha_smos.DTO.PageResponse;
 import com.nha.nha_smos.DTO.ProductRequest;
 import com.nha.nha_smos.DTO.ProductResponse;
 import com.nha.nha_smos.Model.ProductModel;
 import com.nha.nha_smos.Service.ProductService;
 import com.nha.nha_smos.Util.ApiResponse;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -44,9 +48,33 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<ProductResponse>> deleteProduct(@PathVariable int id){
+    public ResponseEntity<ApiResponse<PageResponse<ProductResponse>>> deleteProduct(@PathVariable int id){
         this.productService.delete(id);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(null, "Product deleted successfully"));
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<ApiResponse<PageResponse<ProductResponse>>> listByFilter(
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false) String productName,
+            @RequestParam(required = false) Boolean status,
+            @RequestParam(required = false) String code,
+            @RequestParam(required = false) @DateTimeFormat(iso= DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso= DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) String sortAs,
+            @RequestParam(required = false, defaultValue = "1") int page,
+            @RequestParam(required = false, defaultValue = "10") int size
+
+
+    ){
+            PageResponse<ProductResponse> filterProduct = this.productService.filter(
+                    id, productName, status, code, startDate, endDate, sortBy, sortAs, page, size
+            );
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.ok(filterProduct));
+
     }
 
 }
